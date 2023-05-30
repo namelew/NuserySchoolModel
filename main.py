2f504b325a2411f5f0ff51e3d242b20d1b87596f
namelew <diogomaciel.cunha@gmail.com>
2023-05-30 08:35:37 -0300

    Adding const to control envoriment

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import numpy as np
+import sys
 from sklearn.model_selection import train_test_split as tts
 from sklearn import preprocessing as pp
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.ensemble import RandomForestClassifier as rfc
+from sklearn.metrics import recall_score as recall
 
 FILE="./nursery.csv"
+CASE="bared"
+KBEST=8
+RCAVERAGE="micro"
+
+# load args
+
+if len(sys.argv) > 1:
+  CASE = sys.argv[1]
 
 # load the dataset
 
@@ -23,9 +33,21 @@ for collumn in df.columns:
 Y = df['reputation']
 X = df.drop(['reputation'], axis=1)
 
-X = SelectKBest(chi2, k=8).fit_transform(X, Y)
+if CASE in ["kbest", "tbest", "hpbest"]:
+  X = SelectKBest(chi2, k=KBEST).fit_transform(X, Y)
 
 X_train, X_test, y_train, y_test = tts(X,Y, test_size=0.2, random_state=42)
 
-print(X,Y)
 # training model
+
+# evalueate results
+if CASE == "bared":
+  forest = rfc()
+  forest.fit(X_train, y_train)
+  y_hat = forest.predict(X_test)
+  print(f"No optimized model\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
+if CASE == "kbest":
+  forest = rfc()
+  forest.fit(X_train, y_train)
+  y_hat = forest.predict(X_test)
+  print(f"Optimized model with Select-{KBEST}-Best\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
