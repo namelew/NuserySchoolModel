3d5eb7c2d5197151135de6ab67630f2a1b2c8b78
namelew <diogomaciel.cunha@gmail.com>
2023-05-30 15:27:08 -0300

    changing code to the trainnig sets find in cv to build de model

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import sys
-from sklearn.model_selection import train_test_split as tts
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing as pp
-from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.ensemble import RandomForestClassifier as rfc
 from sklearn.metrics import recall_score as recall
 
 FILE="./nursery.csv"
 CASE="bared"
-KBEST=8
 KFOLD=7
 RCAVERAGE="micro"
 
@@ -23,8 +20,14 @@ if len(sys.argv) > 1:
 
 df=pd.read_csv(FILE, names=["parents", "has_nurs", "form", "children", "housing", "finance", "social", "health", "reputation"])
 
-# encolding collumns
+# dumping collumns
+for collumn in df.columns:
+  if len(df[collumn].unique()) > 2 and collumn not in ["reputation", "children"]:
+    dump=pd.get_dummies(df[collumn],drop_first=True)
+    df = pd.concat([df,dump],axis=1) # add new columns
+    df.drop([collumn],inplace=True,axis=1) # delete the original attribute
 
+# encolding collumns
 for collumn in df.columns:
   laben=pp.LabelEncoder()
   laben.fit(df[collumn])
@@ -35,41 +38,39 @@ for collumn in df.columns:
 Y = df['reputation']
 X = df.drop(['reputation'], axis=1)
 
-if CASE in ["kbest", "tbest", "hpbest"]:
-  X = SelectKBest(chi2, k=KBEST).fit_transform(X, Y)
+kf = KFold(n_splits=KFOLD, random_state=42, shuffle=True)
+forest = rfc()
 
-if CASE != "tbest":
-  X_train, X_test, y_train, y_test = tts(X,Y, test_size=0.2, random_state=42)
+samples = []
+
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X.values[train_index], X.values[test_index]
+    y_train, y_test = Y[train_index], Y[test_index]
+    forest.fit(X_train, y_train)
+    samples.append({
+      "train": (X.values[train_index], Y[train_index]),
+      "test": (X.values[test_index], Y[test_index]),
+      "score": forest.score(X_test, y_test)
+    })
+
+samples.sort(key=lambda x:x["score"], reverse=True)
+
+if KFOLD%2 == 1:
+  best = round(KFOLD%2)
+else:
+  best = round(KFOLD%2) + 1
+
+X_train = samples[best]["train"][0]
+y_train = samples[best]["train"][1]
+X_test = samples[best]["test"][0] 
+y_test = samples[best]["test"][1]
 
 # training model and evaluate results
 if CASE == "bared":
   forest = rfc()
   forest.fit(X_train, y_train)
   y_hat = forest.predict(X_test)
-  print(f"No optimized model\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
-if CASE == "kbest":
-  forest = rfc()
-  forest.fit(X_train, y_train)
-  y_hat = forest.predict(X_test)
-  print(f"Optimized model with Select-{KBEST}-Best\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
-if CASE == "tbest":
-  kf = KFold(n_splits=KFOLD, random_state=42, shuffle=True)
-  forest = rfc()
-
-  accs = []
-  recalls = []
-
-  for train_index, test_index in kf.split(X):
-      X_train, X_test = X[train_index, :], X[test_index, :]
-      y_train, y_test = Y[train_index], Y[test_index]
-      forest.fit(X_train, y_train)
-      y_hat = forest.predict(X_test)
-      accs.append(forest.score(X_test, y_test))
-      recalls.append(recall(y_hat, y_test, average=RCAVERAGE))
-  accs.sort()
-  recalls.sort()
-
-  print(f"Optimized model with Select-{KBEST}-Best and Cross-Validation\n\tprecision: {accs[0]}\n\trecall: {recalls[0]}")
+  print(f"Model Metrics\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
 if CASE == "hpbest":
   parameters = {
       "n_estimators": [25, 50, 100, 200, 400],
@@ -86,6 +87,7 @@ if CASE == "hpbest":
 
   forestGrid = GridSearchCV(rfc(), parameters)
   forestGrid.fit(X_train, y_train)
-  forest = rfc(forestGrid.best_estimator_)
+  forest = forestGrid.best_estimator_
+  forest.fit(X_train, y_train)
   y_hat = forest.predict(X_test)
-  print(f"Optimized model with Select-{KBEST}-Best and GridSearchCV\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
+  print(f"Optimized model with Select-K-Best and GridSearchCV\n\tprecision: {forest.score(X_test, y_test)}")
