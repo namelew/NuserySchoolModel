8547ef3e67f4b51acddfd71a67555bf83f23a7cf
namelew <diogomaciel.cunha@gmail.com>
2023-05-30 09:22:32 -0300

    adding grid search tunning phase

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import numpy as np
 import sys
 from sklearn.model_selection import train_test_split as tts
 from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing as pp
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.ensemble import RandomForestClassifier as rfc
@@ -70,3 +70,22 @@ if CASE == "tbest":
   recalls.sort()
 
   print(f"Optimized model with Select-{KBEST}-Best and Cross-Validation\n\tprecision: {accs[0]}\n\trecall: {recalls[0]}")
+if CASE == "hpbest":
+  parameters = {
+      "n_estimators": [25, 50, 100, 200, 400],
+      "criterion": ["gini", "entropy", "log_loss"],
+      "max_depth": [5, 25, 50, 100, 200],
+      "min_samples_split": [2, 4, 8, 16, 32],
+      "min_samples_leaf": [1, 2, 3, 4, 5],
+      "min_weight_fraction_leaf": [0.0, 0.1, 0.2, 0.3, 0.4],
+      "max_features": ["sqrt", "log2"],
+      "max_leaf_nodes": [10000, 20000, 30000, 40000, 50000],
+      "min_impurity_decrease": [0.0, 0.01, 0.02, 0.03, 0.04],
+      "random_state": [42],
+  }
+
+  forestGrid = GridSearchCV(rfc(), parameters)
+  forestGrid.fit(X_train, y_train)
+  forest = rfc(forestGrid.best_estimator_)
+  y_hat = forest.predict(X_test)
+  print(f"Optimized model with Select-{KBEST}-Best and GridSearchCV\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
