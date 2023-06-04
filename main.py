892da76bcb0d51e1fd0d9e56ca920bb5b46043dc
namelew <diogomaciel.cunha@gmail.com>
2023-06-04 10:43:38 -0300

    Desisti de executar a GridSearchCV, muito demorado

--- main.py
+++ main.py
@@ -61,30 +61,9 @@ y_train = samples[best]["train"][1]
 X_test = samples[best]["test"][0] 
 y_test = samples[best]["test"][1]
 
-# find the best hyperparameters
-
-parameters = {
-      "n_estimators": [1000, 2000, 3000, 4000, 5000],
-      "criterion": ["gini", "entropy", "log_loss"],
-      "max_depth": [3, 9, 81],
-      "min_samples_split": [5, 25, 125],
-      "min_samples_leaf": [5, 10, 20, 40],
-      "min_weight_fraction_leaf": [0.0, 0.2, 0.4, 0.8],
-      "max_features": ["sqrt", "log2"],
-      "max_leaf_nodes": [50000, 60000, 70000, 80000, 90000],
-      "min_impurity_decrease": [0.0, 0.02, 0.04, 0.08, 0.1],
-      "n_jobs": [-1],
-      "random_state": [42],
-  }
-
-forestGrid = GridSearchCV(rfc(), parameters)
-forestGrid.fit(X_train, y_train)
-
-df = pd.DataFrame(forestGrid.cv_results_)
-df.to_csv('grid_search_results_forest.csv', index=False)
-
 # training model and evaluate results
-forest = forestGrid.best_estimator_
+forest = rfc(n_jobs=-1,random_state=42,)
+
 forest.fit(X_train, y_train)
 y_hat = forest.predict(X_test)
 print(f"Model Metrics\n\tprecision: {forest.score(X_test, y_test)}\n\trecall: {recall(y_hat, y_test, average=RCAVERAGE)}")
